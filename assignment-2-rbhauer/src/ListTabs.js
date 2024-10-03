5fa7463d3209e5cb7ea5e80a96344f098d7baab0
Ryan Hauer <RHAUER@depaul.edu>
2024-10-03 15:16:18 -0500

    tabs working

--- assignment-2-rbhauer/src/ListTabs.js
+++ assignment-2-rbhauer/src/ListTabs.js
@@ -2,26 +2,31 @@ import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
+import {todos} from './todoItems.js';
 
 function ListTabs() {
   return (
     <div className= "List_Tabs">
-        <Tab.Container id="ListTabs" defaultActiveKey="#link1">
+        <Tab.Container id="ListTabs" defaultActiveKey="#link0">
         <Row>
             <Col sm={4}>
             <ListGroup>
-                <ListGroup.Item action href="#link1">
-                Link 1
-                </ListGroup.Item>
-                <ListGroup.Item action href="#link2">
-                Link 2
-                </ListGroup.Item>
+                {todos.map((todos, index) => (
+                    <ListGroup.Item eventKey={index.toString()} key={index} action href = {"#link".concat(index.toString())}>
+                        {todos.title}
+                    </ListGroup.Item>
+               ))};
             </ListGroup>
             </Col>
             <Col sm={8}>
             <Tab.Content>
-                <Tab.Pane eventKey="#link1">Tab pane content 1</Tab.Pane>
-                <Tab.Pane eventKey="#link2">Tab pane content 2</Tab.Pane>
+                {todos.map((todos,index) => (
+                    <Tab.Pane eventKey={index.toString()}>
+                        {todos.description}
+                        </Tab.Pane>
+                ))};
+                
+                <Tab.Pane eventKey="#link1">Tab pane content 2</Tab.Pane>
             </Tab.Content>
             </Col>
         </Row>
